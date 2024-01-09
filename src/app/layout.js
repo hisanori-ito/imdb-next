aab11172f911da36789441940ca3f8b20549bea5
Hisanori Ito <86972518+hisanori-ito@users.noreply.github.com>
2024-01-10 01:32:00 +0900

    [add] Navbar components

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import Header from '@/components/Header'
 import Providers from './Providers'
+import NavBar from '@/components/NavBar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +17,8 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <Providers>
           <Header />
-          {children}
+            <NavBar />
+              {children}
         </Providers>
       </body>
     </html>
